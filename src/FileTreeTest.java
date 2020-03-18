66736e617137fec5bfe8236cb501b1251d04aa2c
Drew Halverson <drew.m.halverson@gmail.com>
2020-03-18 16:13:58 -0500

    Added contains() method testing

--- src/FileTreeTest.java
+++ src/FileTreeTest.java
@@ -6,10 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class FileTreeTest {
 
+	protected FileTree tree1;
+	protected final FileNode zebra = new FileNode("/Users/drewhalverson/OneDrive - "
+			+ "UW-Madison/Github/File_Backup_Tools/files_for_testing/zebra.rtf", null);
+	protected final FileNode doggy = new FileNode("/Users/drewhalverson/OneDrive - "
+			+ "UW-Madison/Github/File_Backup_Tools/files_for_testing/doggy.pdf", null);
+	protected final FileNode Doggy = new FileNode("/Users/drewhalverson/OneDrive - "
+			+ "UW-Madison/Github/File_Backup_Tools/files_for_testing/Doggy.pdf", null);
+	
 	@BeforeEach
 	void setUp() throws Exception {
-		FileTree tree1 = new FileTree("/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree1");
-		System.out.println("Hi");
+		tree1 = new FileTree("/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree1");
 	}
 
 	@AfterEach
@@ -25,5 +32,22 @@ class FileTreeTest {
 		assertEquals(FileTree.compareStrings("dog", "Doggo"), 4);
 		assertEquals(FileTree.compareStrings("hidoggo", "Doggo"), 1);
 	}
-
+	
+	/**
+	 * Checks to see the results of several calls to the contains() method to see if they are correct.
+	 */
+	@Test
+	void test002_containsTests() {
+		try {
+			tree1.contains(null);
+			fail("IllegalArgumentException should have been thrown.");
+		}
+		catch(IllegalArgumentException e) {
+			//expected
+		}
+		
+		assertTrue(tree1.contains(zebra));
+		assertTrue(tree1.contains(doggy));
+		assertFalse(tree1.contains(Doggy));
+	}
 }
