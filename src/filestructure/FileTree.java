ba417fb313e731dd9f3d4e1cdc8a76df22072343
Drew Halverson <drew.m.halverson@gmail.com>
2020-05-10 13:08:48 -0500

    Added duplicate file method, and more
    Added several helper methods and a findDuplicateFiles() method that will be used to find duplicate files in a FileTree. Right now there is not yet a way to call it from the main program, that has not been implemented yet. But, that will be added soon after more testing is done. Read documentation above each method for more information.

--- src/filestructure/FileTree.java
+++ src/filestructure/FileTree.java
@@ -2,6 +2,7 @@ package filestructure;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A tree of Nodes that represents a file system in a computer.
@@ -122,21 +123,20 @@ public class FileTree {
 
 		while (!nodesToTraverse.isEmpty() && nodesToTraverse != null) {
 			if (FolderNode.isFolderNode(nodesToTraverse.get(0))) {
-				//computing score and index and using it to add Folder's children:
+				// computing score and index and using it to add Folder's children:
 				int index;
 				temp = (FolderNode) nodesToTraverse.get(0);
-				if(compareStrings(temp.getParentPathString(), temp.getPath().getName(), 
-						file.getParentPathString(), file.getPath().getName()) >= 3) {
+				if (compareStrings(temp.getParentPathString(), temp.getPath().getName(), file.getParentPathString(),
+						file.getPath().getName()) >= 3) {
 					index = 0;
-				}
-				else {
-					index = nodesToTraverse.size()-1;
-					if(index < 0)
+				} else {
+					index = nodesToTraverse.size() - 1;
+					if (index < 0)
 						index = 0;
 				}
 				nodesToTraverse.remove(0);
 				addChildrenList(nodesToTraverse, temp.getChildren(), index);
-				
+
 			} else { // FileNode was found
 				temp2 = (FileNode) nodesToTraverse.get(0);
 				if (temp2.equals(file)) {
@@ -144,16 +144,17 @@ public class FileTree {
 				}
 				nodesToTraverse.remove(0);
 			}
-			
+
 		}
 
 		return false;
 	}
-	
+
 	/**
-	 * Another version of the contains method; this method searches for a FileNode that points to a file that 
-	 * has the same file name as the parameter fileName passed to this method. Does not worry about file size
-	 * unlike the contains() method.
+	 * Another version of the contains method; this method searches for a FileNode
+	 * that points to a file that has the same file name as the parameter fileName
+	 * passed to this method. Does not worry about file size unlike the contains()
+	 * method.
 	 * 
 	 * @param fileName the String of the filename being searched for.
 	 * @return returns true if this fileName is found, false otherwise.
@@ -170,20 +171,19 @@ public class FileTree {
 
 		while (!nodesToTraverse.isEmpty() && nodesToTraverse != null) {
 			if (FolderNode.isFolderNode(nodesToTraverse.get(0))) {
-				//computing score and index and using it to add Folder's children:
+				// computing score and index and using it to add Folder's children:
 				int index;
 				temp = (FolderNode) nodesToTraverse.get(0);
-				if(compareStrings(temp.getPath().getName(), fileName) >= 3) {
+				if (compareStrings(temp.getPath().getName(), fileName) >= 3) {
 					index = 0;
-				}
-				else {
-					index = nodesToTraverse.size()-1;
-					if(index < 0)
+				} else {
+					index = nodesToTraverse.size() - 1;
+					if (index < 0)
 						index = 0;
 				}
 				nodesToTraverse.remove(0);
 				addChildrenList(nodesToTraverse, temp.getChildren(), index);
-				
+
 			} else { // FileNode was found
 				temp2 = (FileNode) nodesToTraverse.get(0);
 				if (temp2.getPath().getName().equals(fileName)) {
@@ -205,11 +205,12 @@ public class FileTree {
 	 * Precondition: index must be >= 0 and < nodesToTraverse.size()
 	 * 
 	 * @param nodesToTraverse the list of nodes that are being traversed.
-	 * @param newChildren the list of nodes being added to nodesToTraverse
-	 * @param index the index of nodesToTraverse at which the newChildren items will be added.
+	 * @param newChildren     the list of nodes being added to nodesToTraverse
+	 * @param index           the index of nodesToTraverse at which the newChildren
+	 *                        items will be added.
 	 */
 	private static void addChildrenList(ArrayList<Node> nodesToTraverse, ArrayList<Node> newChildren, int index) {
-		for(int i = 0; i<newChildren.size(); i++) {
+		for (int i = 0; i < newChildren.size(); i++) {
 			nodesToTraverse.add(index, newChildren.get(i));
 		}
 	}
@@ -239,6 +240,218 @@ public class FileTree {
 			nodesToTraverse.remove(0);
 		}
 		return extraFiles;
+	}
+
+	/**
+	 * Finds duplicate files in this tree. Use the overloaded other version of this
+	 * method if you want to worry about which duplicate file will be added to the
+	 * returned list.
+	 * 
+	 * @return returns an ArrayList of Files that are found twice in this tree; only
+	 *         one of each pair of duplicates will be in this list.
+	 */
+	public ArrayList<File> findDuplicateFiles() {
+		return findDuplicateFiles(null);
+	}
+
+	/**
+	 * Finds duplicate files in this tree. When one is found, it checks to see if
+	 * the two duplicates are in the same path as the otherTree. If one of them is
+	 * in the exact same place, it marks the one that is not in the same place as a
+	 * duplicate, so that if it is deleted the one in the same place as the
+	 * otherTree will be left untouched. In the case that neither are in the same
+	 * place in the otherTree, then either file may be marked as duplicate.
+	 * 
+	 * -Note: ignores ".DS_Store" files from Mac systems.
+	 * 
+	 * @param otherTree another FileTree object to check if files are in the same
+	 *                  position as this one (see above) - null is acceptable
+	 * @return returns an ArrayList of Files that are found twice in this tree; only
+	 *         one of each pair of duplicates will be in this list.
+	 */
+	public ArrayList<File> findDuplicateFiles(FileTree otherTree) {
+		ArrayList<File> duplicates = new ArrayList<File>();
+		HashMap<FileNode, FileNode> seen = new HashMap<FileNode, FileNode>();
+
+		@SuppressWarnings("unchecked")
+		ArrayList<Node> nodesToTraverse = (ArrayList<Node>) this.root.getChildren().clone();
+		FolderNode temp;
+		FileNode temp2;
+
+		while (!nodesToTraverse.isEmpty() && nodesToTraverse != null) {
+			if (FolderNode.isFolderNode(nodesToTraverse.get(0))) { // FolderNode found; add children
+				temp = (FolderNode) nodesToTraverse.get(0);
+				nodesToTraverse.remove(0);
+				addChildrenList(nodesToTraverse, temp.getChildren(), 0);
+
+			} else { // FileNode was found
+				temp2 = (FileNode) nodesToTraverse.get(0);
+
+				if (!temp2.getPath().getName().equals(".DS_Store")) {
+					if (!seen.containsValue(temp2)) { // this FileNode hasn't been seen
+						seen.put(temp2, temp2); // add to seen
+
+					} else { // filenode has been seen, it is duplicate!
+						if (otherTree == null) {
+							duplicates.add(temp2.getPath());
+						} else {
+							// if temp2 is in the same spot in otherTree, add File from seen
+							if (isFileAtPath(temp2, getPathAfterThisTree(temp2))) {
+								duplicates.add(seen.get(temp2).getPath());
+							}
+							// else, add temp2
+							else {
+								duplicates.add(temp2.getPath());
+							}
+						}
+					}
+				}
+				nodesToTraverse.remove(0);
+			}
+		}
+
+		return duplicates;
+	}
+
+	/**
+	 * Returns the path to file beginning after the folder at root of this tree. The
+	 * 
+	 * String returned does not begin with a slash, instead begins with the first
+	 * folder in the path to the file, or if file was in root folder, then just
+	 * displays file name.
+	 * 
+	 * Looks for the root folder with a slash before and after it in the path of the
+	 * file parameter. Accounts for either forward or back slashes depending on
+	 * operating system.
+	 * 
+	 * Precondition: path parameter must be a path in this tree, IE the root must be
+	 * somewhere in the path, AND there must be a slash directly after it.
+	 * 
+	 * If path does not have the root of this tree somewhere in it, null will be
+	 * returned.
+	 * 
+	 * @param path the file to get the path of.
+	 * @return the path to file beginning after the folder at root of this tree.
+	 */
+	protected String getPathAfterThisTree(String path) {
+		if (path.lastIndexOf("/" + this.getRoot().getPath().getName() + "/") < 0) {
+			return null;
+		}
+
+		int startIndex = path.lastIndexOf("/" + this.getRoot().getPath().getName() + "/") + 2
+				+ this.getRoot().getPath().getName().length();
+
+		if (startIndex < 0) { // meaning forward slash was not found, try backward slash in the path
+			startIndex = path.lastIndexOf("\\" + this.getRoot().getPath().getName() + "\\") + 2
+					+ this.getRoot().getPath().getName().length();
+		}
+
+		return path.substring(startIndex);
+	}
+
+	/**
+	 * Overloaded version of the method that accepts a FileNode object, and passes
+	 * its path to the main version of the method.
+	 * 
+	 * @param file FileNode object to take path from.
+	 * @return the path to file beginning after the folder at root of this tree.
+	 */
+	protected String getPathAfterThisTree(FileNode file) {
+		return this.getPathAfterThisTree(file.getPath().getPath());
+	}
+
+	/**
+	 * Checks to see if *file* is at the path given from *path*.
+	 * 
+	 * Protected to make sure valid paths are passed to this method.
+	 * 
+	 * Precondition: path must begin with first folder after the root of this tree
+	 * or the file if it is in the root folder. Root folder should not be included
+	 * in path String. (See getPathAfterThisTree() method, should be called on path
+	 * before)
+	 * 
+	 * If path is null, false will always be returned.
+	 * 
+	 * @param file FileNode to search for
+	 * @param path path to look for file at
+	 * @return true if file was found at the end of path, false otherwise.
+	 */
+	protected boolean isFileAtPath(FileNode file, String path) {
+		if (path == null) {
+			return false;
+		}
+
+		path = FileTree.replaceBackSlashWithForward(path);
+
+		String[] splitPath = path.split("/");
+		FolderNode curFolder = this.getRoot();
+		ArrayList<Node> curFolderChildren;
+
+		// iterate through FileTree DS nodes and follow the path
+		for (int i = 0; i < splitPath.length; i++) {
+			curFolderChildren = curFolder.getChildren();
+
+			// if done (on the last element in splitPath, the filename)
+			if (i == splitPath.length - 1) {
+				if (!curFolder.contains(file)) {
+					return false;
+				}
+			}
+
+			// else, not done, keep iterating
+			else {
+				// Check to make sure curFolder has the next folder in the sequence, and then
+				// change curFolder to that next folder.
+				if (!curFolder.contains(new FolderNode(splitPath[i], null))) {
+					return false;
+				}
+
+				// change curFolder
+				Node tempNode;
+				for (int j = 0; j < curFolder.getChildren().size(); j++) {
+
+					tempNode = curFolder.getChildren().get(j);
+					if (FolderNode.isFolderNode(tempNode)) {
+						if (((FolderNode) tempNode).equalsName(new FolderNode(splitPath[i], null))) {
+							curFolder = (FolderNode) tempNode;
+							j = curFolder.getChildren().size() + 1;
+						}
+					}
+				}
+
+			}
+		}
+
+		return true; // if false wasn't already returned, everything checked out.
+	}
+
+	/**
+	 * Overloaded version of this method that accepts a java File object as input.
+	 * 
+	 * Read other version of method for more details.
+	 * 
+	 * @param file the File object that will be used to create a FileNode object.
+	 * @param path the String path.
+	 * @return true if file was found at the end of path, false otherwise.
+	 */
+	protected boolean isFileAtPath(File file, String path) {
+		return this.isFileAtPath(new FileNode(file, null), path);
+	}
+
+	/**
+	 * Replaces \ back slashes with / forward slashes found anywhere in the path
+	 * String.
+	 * 
+	 * @param path the string being looked at
+	 * @return path with only forward slashes.
+	 */
+	public static String replaceBackSlashWithForward(String path) {
+		for (int i = 0; i < path.length(); i++) {
+			if (path.substring(i, i + 1).equals("\\")) {
+				path = path.substring(0, i) + "/" + path.substring(i + 1);
+			}
+		}
+		return path;
 	}
 
 	/**
@@ -276,16 +489,17 @@ public class FileTree {
 
 		return result;
 	}
-	
+
 	/**
-	 * Overloaded version of the method: accepts parent strings of the first and second strings. These will
-	 * be used for an additional test of similarity.
+	 * Overloaded version of the method: accepts parent strings of the first and
+	 * second strings. These will be used for an additional test of similarity.
 	 * 
-	 * @param firstParent parent string of first
-	 * @param first child string of firstParent
+	 * @param firstParent  parent string of first
+	 * @param first        child string of firstParent
 	 * @param secondParent parent string of second
-	 * @param second child string of secondParent
-	 * @return returns an int score of similarity, 0 being the lowest possible score.
+	 * @param second       child string of secondParent
+	 * @return returns an int score of similarity, 0 being the lowest possible
+	 *         score.
 	 */
 	protected static int compareStrings(String firstParent, String first, String secondParent, String second) {
 		return compareStrings(first, second) + compareStrings(firstParent, secondParent);
--- src/filestructure/FileTreeTest.java
+++ src/filestructure/FileTreeTest.java
@@ -2,26 +2,46 @@ package filestructure;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FileTreeTest {
 
-	protected FileTree tree1;
-	protected final FileNode zebra = new FileNode(
+	protected static FileTree tree2;
+	protected static FileTree tree1;
+
+	protected static final FileNode zebra = new FileNode(
 			"/Users/drewhalverson/OneDrive - " + "UW-Madison/Github/File_Backup_Tools/files_for_testing/zebra.rtf",
 			null);
-	protected final FileNode doggy = new FileNode(
+	protected static final FileNode doggy = new FileNode(
 			"/Users/drewhalverson/OneDrive - " + "UW-Madison/Github/File_Backup_Tools/files_for_testing/doggy.pdf",
 			null);
-	protected final FileNode Doggy = new FileNode(
+	protected static final FileNode Doggy = new FileNode(
 			"/Users/drewhalverson/OneDrive - " + "UW-Madison/Github/File_Backup_Tools/files_for_testing/Doggy.pdf",
 			null);
+
+	protected static final FolderNode tree1Folder = new FolderNode(
+			"/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree1", null);
+	protected static final FolderNode tree2Folder = new FolderNode(
+			"/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree2", null);
+	protected static final FolderNode AppleTree1 = new FolderNode(
+			"/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree1/Apple", tree1Folder);
+	protected static final FolderNode AppleTree2WithTree1ParentNode = new FolderNode(
+			"/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree2/Apple", tree1Folder);
+	protected static final FolderNode AppleTree2 = new FolderNode(
+			"/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree2/Apple", tree2Folder);
+	protected static final FileNode madison = new FileNode(
+			"/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree1/Apple/madison skyline.jpeg",
+			AppleTree1);
 
 	@BeforeEach
 	void setUp() throws Exception {
 		tree1 = new FileTree("/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree1");
+		tree2 = new FileTree("/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree2");
 	}
 
 	@AfterEach
@@ -54,6 +74,39 @@ class FileTreeTest {
 		assertTrue(tree1.contains(zebra));
 		assertTrue(tree1.contains(doggy));
 		assertFalse(tree1.contains(Doggy));
+	}
+
+	/**
+	 * Tests the getPathAfterThisTree() method in FileTree class.
+	 */
+	@Test
+	void test003_PathAfterThisTree() {
+		FileNode doggyInTree1 = new FileNode(
+				new File("/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree1/doggo/doggy.pdf"),
+				null);
+		assertEquals("doggo/doggy.pdf", tree1.getPathAfterThisTree(doggyInTree1));
+	}
+
+	/**
+	 * Tests the isFileAtPath() method in FileTree class.
+	 */
+	@Test
+	void test004_isFileAtPath() {
+		assertFalse(tree1.isFileAtPath(doggy, doggy.getPath().getPath()));
+		assertTrue(tree1.isFileAtPath(doggy, tree1.getPathAfterThisTree(
+				"/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree1/doggo/doggy.pdf")));
+		assertFalse(tree2.isFileAtPath(madison, tree1.getPathAfterThisTree(
+				"/Users/drewhalverson/OneDrive - UW-Madison/Github/File_Backup_Tools/tree2/Apple/madison skyline.jpeg")));
+	}
+	
+	/**
+	 * Tests the duplicate files method with tree2 (pretending this is the backup)
+	 */
+	@Test
+	void test005_duplicateFiles() {
+		ArrayList<File> duplicates = tree2.findDuplicateFiles();
+		
+		assertEquals("doggy.pdf", duplicates.get(0).getName());
 	}
 
 }
